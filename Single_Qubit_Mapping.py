8fe42c34e7c5f2da343ec8022e49baa0e855b62e
joegibs <102627371+joegibs@users.noreply.github.com>
2022-07-21 12:32:17 -0400

    Update Single_Qubit_Mapping.py
    yeet its done

--- Single_Qubit_Mapping.py
+++ Single_Qubit_Mapping.py
@@ -73,19 +73,16 @@ class Experiment:
     Parameters
     ----------
     gate_set : dict, optional
-        DESCRIPTION. The default is CP2.
+        set of gates to use. The default is CP2.
     num_steps : int, optional
-        DESCRIPTION. The default is 1.
+        total number of gates. The default is 1.
     num_sites : int, optional
-        DESCRIPTION. The default is None.
+        number of independent state positions. The default is None.
     init_states : list, optional
-        DESCRIPTION. The default is None.
+        initial position. The default is None.
     gate_list : list, optional
-        DESCRIPTION. The default is None.
-
-    Returns
-    -------
-    None.
+        List of gates to be applied, if not suppplied will generate random
+        gates from gate set. The default is None.
 
     """
 
@@ -104,34 +101,32 @@ class Experiment:
         if num_sites == None and init_states == None:
             self.states = [np.array([0, 1])]
         elif init_states == None:
+            """TODO make random init positions"""
             self.states = [np.array([0, 1]) for x in range(num_sites)]
         elif num_sites == None:
             self.states = [np.array(x) for x in init_states]
 
         if gate_list == None:
-            self.gate_list = self.gen_gate_list()
+            self.gate_list = self.__gen_gate_list()
         else:
             self.num_steps = len(gate_list)
             self.gate_list = gate_list
-        self.gate_list.insert(0, np.identity(2))
+        # self.gate_list.insert(0, np.identity(2))
 
         self.angles = []
 
-        # self.final_unitary = self.gen_final_unitary()
+        # self.final_unitary = self.__gen_final_unitary()
 
-    """
-    Potentially slow but generates everything at once
-    """
-
-    def run(self):
-        self.intermediate_states = self.gen_intermediate_states()
-        self.angles = self.gen_angles()
-
-    def gen_gate_list(self):
+    def __gen_gate_list(self):
         vals = list(self.gate_set.values())
         return [random.choice(vals) for x in range(self.num_steps)]
 
-    def gen_final_unitary(self):
+    ################# run functions ##################################
+    def run(self):
+        self.intermediate_states = self.__gen_intermediate_states()
+        self.angles = self.__gen_angles()
+
+    def __gen_final_unitary(self):
         """
         performs matrix mutiplication to compose total unitary
         from multiplcation of randomly selected gates
@@ -143,18 +138,25 @@ class Experiment:
             gate_step_i = np.matmul(gate_step_j, gate_step_i)
         return gate_step_i
 
-    def gen_intermediate_states(self):
-        return [[np.matmul(y, x) for y in self.gate_list] for x in self.states]
+    def __gen_intermediate_states(self):
+        inter_gate_list = self.gen_intermediate_unitaries()
+        return [[np.matmul(y, x) for y in inter_gate_list] for x in self.states]
 
-    def gen_final_state(self):
-        return [np.matmul(self.final_unitary, x) for x in self.states]
+    def gen_intermediate_unitaries(self):
+        unitary_arr = [np.identity(2)]
+        for i in range(1, self.num_steps):
+            unitary_arr.append(np.matmul(unitary_arr[i - 1], self.gate_list[i]))
+        return unitary_arr
 
-    def gen_angles(self):
+    # def __gen_final_state(self):
+    #     return [np.matmul(self.final_unitary, x) for x in self.states]
+
+    def __gen_angles(self):
         return [
-            [self.get_angle(state) for state in x] for x in self.intermediate_states
+            [self.__get_angle(state) for state in x] for x in self.intermediate_states
         ]
 
-    def get_angle(self, state):
+    def __get_angle(self, state):
         """
         get an angle for a single state
         """
@@ -167,71 +169,54 @@ class Experiment:
 
         return [theta, phi]
 
+    ##############plot functions ########################
+    def plot(self):
+        states_xyz = [self.__gen_xyz_points(x) for x in self.angles]
+        print(np.shape(states_xyz))
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+        u, v = np.mgrid[0 : 2 * np.pi : 50j, 0 : np.pi : 50j]
+        xs = np.cos(u) * np.sin(v)
+        ys = np.sin(u) * np.sin(v)
+        zs = np.cos(v)
+
+        ax.plot_surface(xs, ys, zs, color="lightgrey", alpha=0.3)
+        for x in states_xyz:
+            ax.scatter(x[0], x[1], x[2], marker="o", alpha=0.9)
+        # ax.scatter(xin, yin, zin, marker="o", color="red")
+        plt.show()
+
+    def __gen_xyz_points(self, angle_arr):
+        x = np.array([self.__get_x(angle_arr[0], angle_arr[1])])
+        y = np.array([self.__get_y(angle_arr[0], angle_arr[1])])
+        z = np.array([self.__get_z(angle_arr[0], angle_arr[1])])
+
+        for k in range(len(angle_arr) // 2 - 1):
+            x = np.append(
+                x, np.array([self.__get_x(angle_arr[2 * k + 2], angle_arr[2 * k + 3])])
+            )
+            y = np.append(
+                y, np.array([self.__get_y(angle_arr[2 * k + 2], angle_arr[2 * k + 3])])
+            )
+            z = np.append(
+                z, np.array([self.__get_z(angle_arr[2 * k + 2], angle_arr[2 * k + 3])])
+            )
+
+        return x, y, z
+
+    def __get_x(self, theta, phi):
+        return np.cos(phi) * np.sin(theta)
+
+    def __get_y(self, theta, phi):
+        return np.sin(phi) * np.sin(theta)
+
+    def __get_z(self, theta, phi):
+        return np.cos(theta)
+
 
 #%%
-exp = Experiment(num_steps=5)
+n = np.sqrt(1 / 2)
+init_state = [[1, 0], [0, 1], [n, n]]
+exp = Experiment(init_states=init_state, num_steps=200)
 exp.run()
-
-#%%
-"""
-Converts the two polar angles to 3D cartesean coordinates to be plotted
-against the unit sphere
-
-    angle_arr: list of polar angle pairs, usually called with arguement
-    self.angles
-"""
-
-
-def U2_to_03Graph(angle_arr):
-
-    x = np.array([get_x(angle_arr[0], angle_arr[1])])
-    y = np.array([get_y(angle_arr[0], angle_arr[1])])
-    z = np.array([get_z(angle_arr[0], angle_arr[1])])
-
-    for k in range(len(angle_arr) // 2 - 1):
-
-        x = np.append(x, np.array([get_x(angle_arr[2 * k + 2], angle_arr[2 * k + 3])]))
-        y = np.append(y, np.array([get_y(angle_arr[2 * k + 2], angle_arr[2 * k + 3])]))
-        z = np.append(z, np.array([get_z(angle_arr[2 * k + 2], angle_arr[2 * k + 3])]))
-
-    plt.rcParams["figure.figsize"] = [20.00, 20.00]
-    plt.rcParams["figure.autolayout"] = True
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    u, v = np.mgrid[0 : 2 * np.pi : 50j, 0 : np.pi : 50j]
-    xs = np.cos(u) * np.sin(v)
-    ys = np.sin(u) * np.sin(v)
-    zs = np.cos(v)
-    xin = [0]
-    yin = [0]
-    zin = [1]
-
-    """
-    First plot is of the bloch sphere, second is of all the generated data
-    third is for labelling the state |0>
-    """
-
-    ax.plot_surface(xs, ys, zs, color="lightgrey", alpha=0.3)
-    ax.scatter(x, y, z, marker="o", color="black", alpha=0.9)
-    ax.scatter(xin, yin, zin, marker="o", color="red")
-    plt.show()
-
-
-"""
-Functions for getting cartesean coordinates from polar angles
-"""
-
-
-def get_x(theta, phi):
-
-    return np.cos(phi) * np.sin(theta)
-
-
-def get_y(theta, phi):
-
-    return np.sin(phi) * np.sin(theta)
-
-
-def get_z(theta, phi):
-
-    return np.cos(theta)
+exp.plot()
